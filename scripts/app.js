de8dc46447bdd26b806fe5e5f9d98c0c118c7853
NrupM <maryenorthrup@gmail.com>
2017-07-10 18:53:00 -0700

    included animate method to move players across the screen

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,25 @@
-$(document).ready(function(){
-  var player1
-  var player2
-  var position1
-  var position2
-
-
-
-}
-
-//event listener for keypress player1
-//event listener for keypress player2
-
-//track player1 position
-//track player2 position
-
-//check for win
+// $(document).ready(function(){
+//   var player1
+//   var player2
+//   var position1
+//   var position2
+//   var gameOn = false;
+//
+//
+//
+// }
+//
+// //event listener for keypress player1
+// //event listener for keypress player2
+//
+// //track player1 position
+// //track player2 position
+//
+// //check for win
+//
+// function movePlayer(){
+//
+// }
+//
+//
+// //click right arrow key == move Wall-E to the right one step
