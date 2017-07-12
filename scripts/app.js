1f2ae9951bf21bed863aa4498e370eeef6e05de2
NrupM <maryenorthrup@gmail.com>
2017-07-12 08:58:51 -0700

    removed reset button

--- scripts/app.js
+++ scripts/app.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
   //event listener for gameOn
   $("*").keydown(function(e) {
-    if(e.which ===32 && gameOn === false){
+    if(e.which === 32 && gameOn === false){
       e.preventDefault();
       gameOn = true;
       $("#instructions").addClass("hidden");
@@ -15,10 +15,7 @@ $(document).ready(function() {
     }
   });
 
-  $("#reset").click(function(e) {
-        e.preventDefault();
-        reset();
-      });
+// reset function
 
   function reset() {
     player1.animate({
@@ -31,22 +28,28 @@ $(document).ready(function() {
     position1 = 0;
     position2 = 0;
     gameOn = false;
+
   }
+
+
+  $("#reset").click(function(e) {
+    reset();
+  });
 
   $("body").keydown(function(e) {
     if (e.which === 39 && gameOn === true) {
+      position1 += 1;
       $("#player1").animate({
         left: "+=100px"
       });
-      position1 += 5;
       console.log(position1);
     }
 
     if (e.which === 90 && gameOn === true) {
+      position2 += 1;
       $("#player2").animate({
         left: "+=100px"
       });
-      position2 += 5;
       console.log(position2);
     }
 
@@ -54,12 +57,12 @@ $(document).ready(function() {
   });
 
   function evaluateWin() {
-    if (position1 === 55 && position2 !== 55) {
+    if (position1 >= 9 && position2 !== 9) {
       console.log("walle should be winnng")
       $("#instructions").removeClass("hidden");
       $("#instructions").text("Wall-E Wins!!");
       reset();
-    } else if (position2 === 55 && position1 !== 55) {
+    } else if (position2 >= 9 && position1 !== 9) {
       console.log("eve wins")
       $("#instructions").removeClass("hidden");
       $("#instructions").text("Eve Wins!!");
